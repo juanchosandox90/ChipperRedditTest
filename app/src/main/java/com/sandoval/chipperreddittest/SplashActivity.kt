f70a7f40f27cefd2170bb4c54ef1ee8232c2a190
juan.sandoval <Naruto2790>
2020-02-26 16:37:05 -0500

    feature_002_SplashLayoutDesign  - Splash Created for the application  - Add lottie animation for the splash  - SplashScreen launch activity then PostList  - SplashActivity extends from BaseActivity

--- app/src/main/java/com/sandoval/chipperreddittest/SplashActivity.kt
+++ app/src/main/java/com/sandoval/chipperreddittest/SplashActivity.kt
@@ -4,18 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import com.sandoval.chipperreddittest.base.BaseActivity
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity() {
 
-    private val SPLASH_TIME_OUT: Long = 3000 // 1 sec
+    private val SPLASH_TIME_OUT: Long = 3000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-
         Handler().postDelayed({
             startActivity(Intent(this,MainActivity::class.java))
             finish()
         }, SPLASH_TIME_OUT)
     }
+
+    override val layoutResId: Int = R.layout.activity_splash
 }
