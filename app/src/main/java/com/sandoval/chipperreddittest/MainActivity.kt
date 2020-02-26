20442262ea9c678316545e9f5fd9c85bbc7a70f2
juan.sandoval <Naruto2790>
2020-02-26 15:12:03 -0500

    feature_001_FirstPostListFragmentLayoutDesign  - Navigation graph created  - Libs created to be used in the future  - Added progress bar for fetching reddits

--- app/src/main/java/com/sandoval/chipperreddittest/MainActivity.kt
+++ app/src/main/java/com/sandoval/chipperreddittest/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.sandoval.chipperreddittest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sandoval.chipperreddittest.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbarMain)
     }
+
+    override val layoutResId: Int = R.layout.activity_main
 }
